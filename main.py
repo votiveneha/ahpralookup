ba815222bd9e64fa5a3049b86a3c4c949d2aced2
votiveneha <votivephp.neha@gmail.com>
2025-06-13 14:17:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ driver.get("https://www.ahpra.gov.au/Registration/Registers-of-Practitioners.asp
 time.sleep(2)
 
 driver.find_element(By.ID, "name-reg").send_keys("NMW0001234567")
-driver.find_element(By.ID, "search-button").click()
+driver.find_element(By.ID, "predictiveSearchHomeBtn").click()
 time.sleep(3)
 
 html = driver.page_source
